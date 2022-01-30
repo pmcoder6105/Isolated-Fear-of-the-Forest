9ef89cf88d6660ad4e64f2679bfc5693dc07fabe
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-01-30 14:01:51 -0800

    Added outside door and functionality
    Still have to playtest

--- 6 Hours/Assets/Transporter.cs
+++ 6 Hours/Assets/Transporter.cs
@@ -2,19 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KitchenTransporter : MonoBehaviour
+public class Transporter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnCollisionEnter(Collision other)
     {
@@ -25,10 +14,30 @@ public class KitchenTransporter : MonoBehaviour
             transform.position = new Vector3(-10.14f, -1.595f, 9.618f);
         }
 
-        if (other.gameObject.tag == "LivingRoomDoor")
+        if (other.gameObject.tag == "KitchenLivingRoomDoor")
         {
             Debug.Log("Joe mama");
             transform.position = new Vector3(-5.453f, -1.595f, 9.618f);
+        }
+        if (other.gameObject.tag == "BedRoomDoor")
+        {
+            Debug.Log("Joe mama's mama");
+            transform.position = new Vector3(2.718f, -1.769f, 20.08f);
+        }
+        if (other.gameObject.tag == "BedLivingRoomDoor")
+        {
+            Debug.Log("Joe mama's mama's mama");
+            transform.position = new Vector3(3.119f, -1.769f, 16.702f);
+        }
+        if (other.gameObject.tag == "BackInsideDoor")
+        {
+            Debug.Log("Joe mama's mama's mama");
+            transform.position = new Vector3(-0.348f, -1.473f, 2.35f);
+        }
+        if (other.gameObject.tag == "GoOutsideDoor")
+        {
+            Debug.Log("Joe mama's mama's mama");
+            transform.position = new Vector3(-0.12f, -1.61f, -0.4f);
         }
     }
 }
