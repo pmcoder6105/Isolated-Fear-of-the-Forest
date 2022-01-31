ead1ea5182ba2140e59b2da5fceaa24f0a5acd40
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-01-30 16:06:39 -0800

    Added cool animation of lighting to the rest of the rooms
    Next:
    
    Add objective for cardboard boxes

--- 6 Hours/Assets/Transporter.cs
+++ 6 Hours/Assets/Transporter.cs
@@ -6,6 +6,10 @@ public class Transporter : MonoBehaviour
 {
     [SerializeField] GameObject LRLight;
     [SerializeField] GameObject LRLightGoingIntoRoom;
+    [SerializeField] GameObject KRLight;
+    [SerializeField] GameObject KRLightGoingIntoRoom;
+    [SerializeField] GameObject BRLight;
+    [SerializeField] GameObject BRLightGoingIntoRoom;
 
     void Start()
     {
@@ -21,23 +25,32 @@ public class Transporter : MonoBehaviour
             Invoke(nameof(GoIntoKitchen), 2);
             LRLightGoingIntoRoom.SetActive(true);
             LRLight.SetActive(false);
-            Invoke(nameof(TurnLightBacknormal), 2);
+            Invoke(nameof(TurnLightBackNormal), 2);
         }
 
         if (other.gameObject.tag == "KitchenLivingRoomDoor")
         {
             Debug.Log("Joe mama");
-            transform.position = new Vector3(-5.453f, -1.595f, 9.618f);
+            Invoke(nameof(GoIntoLivingRoomFromKitchen), 2);
+            KRLightGoingIntoRoom.SetActive(true);
+            KRLight.SetActive(false);
+            Invoke(nameof(TurnKRLightBackNormal), 2);
         }
         if (other.gameObject.tag == "BedRoomDoor")
         {
             Debug.Log("Joe mama's mama");
-            transform.position = new Vector3(2.718f, -1.769f, 20.08f);
+            Invoke(nameof(GoIntoBedRoomFromLivingRoom), 2);
+            LRLightGoingIntoRoom.SetActive(true);
+            LRLight.SetActive(false);
+            Invoke(nameof(TurnLightBackNormal), 2);
         }
         if (other.gameObject.tag == "BedLivingRoomDoor")
         {
             Debug.Log("Joe mama's mama's mama");
-            transform.position = new Vector3(3.119f, -1.769f, 16.702f);
+            Invoke(nameof(GoIntoLivingRoomFromBedRoom), 2);
+            BRLightGoingIntoRoom.SetActive(true);
+            BRLight.SetActive(false);
+            Invoke(nameof(TurnBRLightBackNormal), 2);
         }
         if (other.gameObject.tag == "BackInsideDoor")
         {
@@ -51,7 +64,32 @@ public class Transporter : MonoBehaviour
         }
     }
 
-    private void TurnLightBacknormal()
+    private void GoIntoLivingRoomFromBedRoom()
+    {
+        transform.position = new Vector3(3.119f, -1.769f, 16.702f);
+    }
+
+    private void TurnBRLightBackNormal()
+    {
+        BRLight.SetActive(true);
+    }
+
+    private void GoIntoBedRoomFromLivingRoom()
+    {
+        transform.position = new Vector3(2.718f, -1.769f, 20.08f);
+    }
+
+    private void TurnKRLightBackNormal()
+    {
+        KRLight.SetActive(true);
+    }
+
+    private void GoIntoLivingRoomFromKitchen()
+    {
+        transform.position = new Vector3(-5.453f, -1.595f, 9.618f);
+    }
+
+    private void TurnLightBackNormal()
     {
         LRLight.SetActive(true);
     }
