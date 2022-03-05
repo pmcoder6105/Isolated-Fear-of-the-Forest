ba16144c779599e757b5942be17906db744e8c76
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-03-04 16:21:56 -0800

    I figured out a way to fix double door bug

--- 6 Hours/Assets/MyScripts/Transporter.cs
+++ 6 Hours/Assets/MyScripts/Transporter.cs
@@ -18,12 +18,10 @@ public class Transporter : MonoBehaviour
     [SerializeField] GameObject walkingEmpty;
     bool isTransporting = false;
     int doorSound;
-    [SerializeField] Component movement;
 
     void Start()
     {
         aS = GetComponent<AudioSource>();
-        //movement.
     }
 
     void Update()
