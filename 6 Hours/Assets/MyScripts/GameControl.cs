33543b26678b0a74a4b5fa4b6e1f05ac100245e3
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-03-06 15:40:55 -0800

    Renamed Transporter.cs to GameControl.cs
    This would be a more effective name for this script, due to the fact that I will be adding more things than transportation in this script

--- 6 Hours/Assets/MyScripts/GameControl.cs
+++ 6 Hours/Assets/MyScripts/GameControl.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Transporter : MonoBehaviour
+public class GameControl : MonoBehaviour
 {
     [SerializeField] GameObject LRLight;
     [SerializeField] GameObject LRLightGoingIntoRoom;
