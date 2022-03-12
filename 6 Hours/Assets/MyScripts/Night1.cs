b482f2ff59bd69d19c6c85b660d1296ab95ca68e
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-03-11 17:07:14 -0800

    I created a piece of code that will turn animator off
    I will implement this through the animation for the player

--- 6 Hours/Assets/MyScripts/Night1.cs
+++ 6 Hours/Assets/MyScripts/Night1.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class Night1 : MonoBehaviour
 {
+    bool isCutsceneDone = false;
+    Animator aN;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        aN = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isCutsceneDone == true)
+        {
+            aN.enabled = false;
+        }
     }
 }
