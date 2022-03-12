a44312689f28d2a9eb72056468190bf10ce60395
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-03-12 11:20:32 -0800

    Implemented simple bool to check if on trail
    Haven't added jumpscare yet

--- 6 Hours/Assets/MyScripts/Night1.cs
+++ 6 Hours/Assets/MyScripts/Night1.cs
@@ -6,6 +6,7 @@ public class Night1 : MonoBehaviour
 {
     [SerializeField] bool isCutsceneDone = false;
     Animator aN;
+    bool isOnTrail = false;
     
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,30 @@ public class Night1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        EnablePlayerMovementAfterCutscene();
+    }
+
+    void EnablePlayerMovementAfterCutscene()
+    {
         if (isCutsceneDone == true)
         {
             aN.enabled = false;
+        }
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        CheckIfOnTrail(other);
+    }
+
+    void CheckIfOnTrail(Collision other)
+    {
+        if (other.gameObject.tag == "Trail")
+        {
+            isOnTrail = true;
+        }
+        else
+        {
+            isOnTrail = false;
         }
     }
 }
