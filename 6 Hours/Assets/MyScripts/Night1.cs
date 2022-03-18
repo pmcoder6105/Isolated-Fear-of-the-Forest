223a80532791cc893475c04be149f6fbca18ab90
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-03-18 12:53:15 -0700

    Added jumpscare points in the game
    Still haven't added the mechanic yet

--- 6 Hours/Assets/MyScripts/Night1.cs
+++ 6 Hours/Assets/MyScripts/Night1.cs
@@ -10,11 +10,32 @@ public class Night1 : MonoBehaviour
     [SerializeField] GameObject monster;
     [SerializeField] GameObject fadeOut;
     Vector3 monsterPos;
+    int rustle1;
+    int rustle2;
+    int rustle3;
+    int rustle4;
+    int rustle5;
+    int rustle6;
+    int rustle7;
 
     // Start is called before the first frame update
     void Start()
     {
         aN = GetComponent<Animator>();
+        rustle1 = Random.Range(4+24, 24+16);
+        rustle2 = Random.Range(22+24, 24+51);
+        rustle3 = Random.Range(60+24, 24+78);
+        rustle4 = Random.Range(85+24, 24+94);
+        rustle5 = Random.Range(96+24, 24+106);
+        rustle6 = Random.Range(110+24, 24+115);
+        rustle7 = Random.Range(116+24, 24+120);
+        Debug.Log(rustle1);
+        Debug.Log(rustle2);
+        Debug.Log(rustle3);
+        Debug.Log(rustle4);
+        Debug.Log(rustle5);
+        Debug.Log(rustle6);
+        Debug.Log(rustle7);
     }
 
     // Update is called once per frame
