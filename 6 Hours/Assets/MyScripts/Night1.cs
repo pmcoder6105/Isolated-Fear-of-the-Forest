e23bccee628d9f1e3d15c7156525c90a314f915a
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-03-26 11:00:38 -0700

    Might have fixed the bug

--- 6 Hours/Assets/MyScripts/Night1.cs
+++ 6 Hours/Assets/MyScripts/Night1.cs
@@ -33,19 +33,20 @@ public class Night1 : MonoBehaviour
     bool shouldStartTimer = false;
     [SerializeField] GameObject audioTrackingBeginningTimeline;
     [SerializeField] GameObject car;
+    [SerializeField] bool shouldSkipIntro = false;
 
     // Start is called before the first frame update
     void Start()
     {
         aS = GetComponent<AudioSource>();
         aN = GetComponent<Animator>();
-        rustle1 = Random.Range(10+24, 24+16);
-        rustle2 = Random.Range(22+24, 24+51);
-        rustle3 = Random.Range(60+24, 24+78);
-        rustle4 = Random.Range(85+24, 24+94);
-        rustle5 = Random.Range(96+24, 24+106);
-        rustle6 = Random.Range(110+24, 24+115);
-        rustle7 = Random.Range(116+24, 24+120);
+        rustle1 = Random.Range(10 + 24, 24 + 16);
+        rustle2 = Random.Range(22 + 24, 24 + 51);
+        rustle3 = Random.Range(60 + 24, 24 + 78);
+        rustle4 = Random.Range(85 + 24, 24 + 94);
+        rustle5 = Random.Range(96 + 24, 24 + 106);
+        rustle6 = Random.Range(110 + 24, 24 + 115);
+        rustle7 = Random.Range(116 + 24, 24 + 120);
         Debug.Log(rustle1);
         Debug.Log(rustle2);
         Debug.Log(rustle3);
@@ -53,9 +54,19 @@ public class Night1 : MonoBehaviour
         Debug.Log(rustle5);
         Debug.Log(rustle6);
         Debug.Log(rustle7);
-        aN.ForceStateNormalizedTime(1);
-        audioTrackingBeginningTimeline.SetActive(false);
-        car.SetActive(false);
+        DebugKeys();
+    }
+
+    void DebugKeys()
+    {
+        if (shouldSkipIntro == true)
+        {
+            aN.ForceStateNormalizedTime(1);
+            audioTrackingBeginningTimeline.SetActive(false);
+            car.SetActive(false);
+            timer.GetComponent<TMP_Text>().text = "0";
+            timer.GetComponent<TMP_Text>().text = Time.time.ToString();
+        }        
     }
 
     // Update is called once per frame
@@ -162,6 +173,11 @@ public class Night1 : MonoBehaviour
                 Debug.Log("averted jumpscare?");
                 if (!radio.GetComponent<AudioSource>().isPlaying)
                 {
+                    radio.GetComponent<AudioSource>().PlayOneShot(radioSFX);
+                }
+                else if (radio.GetComponent<AudioSource>().isPlaying)
+                {
+                    radio.GetComponent<AudioSource>().Stop();
                     radio.GetComponent<AudioSource>().PlayOneShot(radioSFX);
                 }
                 return;
