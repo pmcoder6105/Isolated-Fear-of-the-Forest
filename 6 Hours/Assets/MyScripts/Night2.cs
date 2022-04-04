b8d7e192c16721ceb09d8f2c335cd1444e3bfd67
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-04-04 10:00:55 -0700

    Did some changes (read desc)
    Made a starting scene, also made some code that will secure the buttons in their spot, (don't know if it works). Also, I need to find a way to play the correct animation clip. for some reason, it is only playing the car crash from the last level

--- 6 Hours/Assets/MyScripts/Night2.cs
+++ 6 Hours/Assets/MyScripts/Night2.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Night2 : MonoBehaviour
 {
+    [SerializeField] GameObject MainScreen;
+    [SerializeField] GameObject StockHomeSupplies;
+    [SerializeField] GameObject OrderCleaningSupplies;
+    [SerializeField] GameObject Appliances;
+    Vector3 MainScreenPos;
+    Vector3 StockHomeSuppliesPos;
+    Vector3 OrderCleaningSuppliesPos;
+    Vector3 AppliancesPos;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        MainScreenPos = MainScreen.transform.position;
+        StockHomeSuppliesPos = StockHomeSupplies.transform.position;
+        OrderCleaningSuppliesPos = OrderCleaningSupplies.transform.position;
+        AppliancesPos = Appliances.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MainScreen.transform.position = MainScreenPos;
+        StockHomeSupplies.transform.position = StockHomeSuppliesPos;
+        OrderCleaningSupplies.transform.position = OrderCleaningSuppliesPos;
+        Appliances.transform.position = AppliancesPos; 
     }
 }
