d2ea603cd94af320654c280e346f7b059935542b
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-04-04 17:37:01 -0700

    Made zoom in and out scene
    Haven't playtested

--- 6 Hours/Assets/MyScripts/Night2.cs
+++ 6 Hours/Assets/MyScripts/Night2.cs
@@ -9,49 +9,39 @@ public class Night2 : MonoBehaviour
     [SerializeField] GameObject OrderCleaningSupplies;
     [SerializeField] GameObject Appliances;
     [SerializeField] GameObject laptop;
-    Vector3 MainScreenPos;
-    Vector3 StockHomeSuppliesPos;
-    Vector3 OrderCleaningSuppliesPos;
-    Vector3 AppliancesPos;
+    //Vector3 MainScreenPos;
+    //Vector3 StockHomeSuppliesPos;
+    //Vector3 OrderCleaningSuppliesPos;
+    //Vector3 AppliancesPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        MainScreenPos = MainScreen.transform.position;
-        StockHomeSuppliesPos = StockHomeSupplies.transform.position;
-        OrderCleaningSuppliesPos = OrderCleaningSupplies.transform.position;
-        AppliancesPos = Appliances.transform.position;
+        //MainScreenPos = MainScreen.transform.position;
+        //StockHomeSuppliesPos = StockHomeSupplies.transform.position;
+        //OrderCleaningSuppliesPos = OrderCleaningSupplies.transform.position;
+        //AppliancesPos = Appliances.transform.position;
         this.gameObject.GetComponent<Animator>().Play("PlayerEnteringRoomNight2", 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        MainScreen.transform.position = MainScreenPos;
-        StockHomeSupplies.transform.position = StockHomeSuppliesPos;
-        OrderCleaningSupplies.transform.position = OrderCleaningSuppliesPos;
-        Appliances.transform.position = AppliancesPos;
-        //if (!UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject(laptop))
-        //{
-        //
-        //}
-        
-    }
-
-    void OnMouseOver()
-    {
-        this.GetComponent<Rigidbody>().isKinematic = true;
-        MainScreen.SetActive(true);
-        StockHomeSupplies.SetActive(true);
-        OrderCleaningSupplies.SetActive(true);
-        Appliances.SetActive(true);
-    }
-    private void OnMouseExit()
-    {
-        this.GetComponent<Rigidbody>().isKinematic = false;
-        MainScreen.SetActive(false);
-        StockHomeSupplies.SetActive(false);
-        OrderCleaningSupplies.SetActive(false);
-        Appliances.SetActive(false);
+        //MainScreen.transform.position = MainScreenPos;
+        //StockHomeSupplies.transform.position = StockHomeSuppliesPos;
+        //OrderCleaningSupplies.transform.position = OrderCleaningSuppliesPos;
+        //Appliances.transform.position = AppliancesPos;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            GetComponent<Animator>().Play("ZoomIntoComputerNight2");
+            GetComponent<CapsuleCollider>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            GetComponent<Animator>().Play("ZoomOutOfComputerNight2");
+            GetComponent<CapsuleCollider>().enabled = true;
+            GetComponent<Rigidbody>().isKinematic = false;
+        }
     }
 }
