f4b81c0db390dd3da760b7975257835a7382188c
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-04-04 19:04:08 -0700

    Did lots of work on the zooming in and out feature
    Haven't actually added any functionality to the task buttons, but will do that tomorrow or today
    
    Most likely tomorrow
    
    Day two is getting very good so far. had some doubts at the very start of day 2 because of all the bugs and confusions, but it looks good now. smooth sailings with the development

--- 6 Hours/Assets/MyScripts/Night2.cs
+++ 6 Hours/Assets/MyScripts/Night2.cs
@@ -9,6 +9,7 @@ public class Night2 : MonoBehaviour
     [SerializeField] GameObject OrderCleaningSupplies;
     [SerializeField] GameObject Appliances;
     [SerializeField] GameObject laptop;
+    bool isLookingAtScreen = false;
     //Vector3 MainScreenPos;
     //Vector3 StockHomeSuppliesPos;
     //Vector3 OrderCleaningSuppliesPos;
@@ -32,16 +33,27 @@ public class Night2 : MonoBehaviour
         //OrderCleaningSupplies.transform.position = OrderCleaningSuppliesPos;
         //Appliances.transform.position = AppliancesPos;
         if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<Animator>().Play("ZoomIntoComputerNight2");
-            GetComponent<CapsuleCollider>().enabled = false;
-            GetComponent<Rigidbody>().isKinematic = true;
+        {                        
+            isLookingAtScreen = !isLookingAtScreen;
+            if (isLookingAtScreen == true)
+            {
+                GetComponent<Animator>().Play("ZoomIntoComputerNight2");
+                GetComponent<CapsuleCollider>().enabled = false;
+                GetComponent<Rigidbody>().isKinematic = true;
+                Invoke(nameof(TurnMainScreenActiveAfterZoomingIn), 1f);
+            }
+            if (isLookingAtScreen == false)
+            {
+                GetComponent<Animator>().Play("ZoomOutOfComputerNight2");
+                GetComponent<CapsuleCollider>().enabled = true;
+                GetComponent<Rigidbody>().isKinematic = false;
+                MainScreen.SetActive(false);
+            }
         }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            GetComponent<Animator>().Play("ZoomOutOfComputerNight2");
-            GetComponent<CapsuleCollider>().enabled = true;
-            GetComponent<Rigidbody>().isKinematic = false;
-        }
+    }
+
+    private void TurnMainScreenActiveAfterZoomingIn()
+    {
+        MainScreen.SetActive(true);
     }
 }
