333c9ea764d1fab91d40e2f936caa25d44eefbcf
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-04-06 12:50:46 -0700

    I'm not going to fix the horizontal bug
    It'll be one of the features of this level
    Also I am still trying to figure out the camera bug

--- 6 Hours/Assets/MyScripts/Night2.cs
+++ 6 Hours/Assets/MyScripts/Night2.cs
@@ -21,7 +21,6 @@ public class Night2 : MonoBehaviour
     {
         ToggleBetweenLaptopAndExteriorView();        
     }
-
     private void ToggleBetweenLaptopAndExteriorView()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -31,8 +30,7 @@ public class Night2 : MonoBehaviour
                 isLookingAtScreen = !isLookingAtScreen;
             }               
             if (isLookingAtScreen == true)
-            {                
-                cam.transform.rotation = Quaternion.Euler(0, 0, 0);                             
+            {                                          
                 GetComponent<Animator>().Play("ZoomIntoComputerNight2");
                 GetComponent<CapsuleCollider>().enabled = false;
                 GetComponent<Rigidbody>().isKinematic = true;
@@ -40,7 +38,6 @@ public class Night2 : MonoBehaviour
             }
             if (isLookingAtScreen == false)
             {
-                cam.transform.rotation = Quaternion.Euler(0, 0, 0);
                 GetComponent<Animator>().Play("ZoomOutOfComputerNight2");
                 GetComponent<CapsuleCollider>().enabled = true;
                 GetComponent<Rigidbody>().isKinematic = false;
@@ -51,7 +48,7 @@ public class Night2 : MonoBehaviour
 
     private void TurnMainScreenActiveAfterZoomingIn()
     {
-        if (GetComponent<Rigidbody>().isKinematic == true)
+        if (GetComponent<Rigidbody>().isKinematic == false)
         {
             MainScreen.SetActive(true);
         }        
