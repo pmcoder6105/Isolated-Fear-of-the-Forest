8e6ae4b7be8d744d9632a486ca9ecd80a9a9dc2c
Pratyush <pratyush.mahadevaiah@gmail.com>
2022-05-29 10:42:21 -0700

    Added scene loader
    Need to connect everything though

--- 6 Hours/Assets/MyScripts/SceneLoader.cs
+++ 6 Hours/Assets/MyScripts/SceneLoader.cs
@@ -1,19 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject loadingScreen;
+    public Slider sliderBar;
+
+    public void LoadScene(int levelIndex)
     {
-        
+        StartCoroutine(LoadSceneAsynchronously(levelIndex));
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadSceneAsynchronously(int levelIndex)
     {
-        
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelIndex);
+        loadingScreen.SetActive(true);
+        while(!operation.isDone)
+        {
+            sliderBar.value = operation.progress;
+            yield return null;
+        }
     }
 }
